8f262704209b396760b56041b86b51ad6c28d512
Torsten Curdt <tcurdt@vafer.org>
2025-08-17 23:24:00 +0200

    changes for the common but broken grblv4 breakout boards

--- grbl/cpu_map.h
+++ grbl/cpu_map.h
@@ -27,7 +27,7 @@
 #define cpu_map_h
 
 
-#ifdef CPU_MAP_ATMEGA328P // (Arduino Uno) Officially supported by Grbl.
+#ifdef CPU_MAP_ATMEGA328P // Arduino Nano on GRBL board v4
 
   // Define serial port pins and interrupt vectors.
   #define SERIAL_RX     USART_RX_vect
@@ -36,23 +36,23 @@
   // Define step pulse output pins. NOTE: All step bit pins must be on the same port.
   #define STEP_DDR        DDRD
   #define STEP_PORT       PORTD
-  #define X_STEP_BIT      2  // Uno Digital Pin 2
-  #define Y_STEP_BIT      3  // Uno Digital Pin 3
-  #define Z_STEP_BIT      4  // Uno Digital Pin 4
+  #define X_STEP_BIT      5  // Nano Digital Pin 5
+  #define Y_STEP_BIT      6  // Nano Digital Pin 6
+  #define Z_STEP_BIT      7  // Nano Digital Pin 7
   #define STEP_MASK       ((1<<X_STEP_BIT)|(1<<Y_STEP_BIT)|(1<<Z_STEP_BIT)) // All step bits
 
   // Define step direction output pins. NOTE: All direction pins must be on the same port.
   #define DIRECTION_DDR     DDRD
   #define DIRECTION_PORT    PORTD
-  #define X_DIRECTION_BIT   5  // Uno Digital Pin 5
-  #define Y_DIRECTION_BIT   6  // Uno Digital Pin 6
-  #define Z_DIRECTION_BIT   7  // Uno Digital Pin 7
+  #define X_DIRECTION_BIT   2  // Nano Digital Pin 2
+  #define Y_DIRECTION_BIT   3  // Nano Digital Pin 3
+  #define Z_DIRECTION_BIT   4  // Nano Digital Pin 4
   #define DIRECTION_MASK    ((1<<X_DIRECTION_BIT)|(1<<Y_DIRECTION_BIT)|(1<<Z_DIRECTION_BIT)) // All direction bits
 
   // Define stepper driver enable/disable output pin.
   #define STEPPERS_DISABLE_DDR    DDRB
   #define STEPPERS_DISABLE_PORT   PORTB
-  #define STEPPERS_DISABLE_BIT    0  // Uno Digital Pin 8
+  #define STEPPERS_DISABLE_BIT    0  // Nano Digital Pin 8
   #define STEPPERS_DISABLE_MASK   (1<<STEPPERS_DISABLE_BIT)
 
   // Define homing/hard limit switch input pins and limit interrupt vectors.
@@ -60,12 +60,12 @@
   #define LIMIT_DDR        DDRB
   #define LIMIT_PIN        PINB
   #define LIMIT_PORT       PORTB
-  #define X_LIMIT_BIT      1  // Uno Digital Pin 9
-  #define Y_LIMIT_BIT      2  // Uno Digital Pin 10
+  #define X_LIMIT_BIT      1  // Nano Digital Pin 9
+  #define Y_LIMIT_BIT      2  // Nano Digital Pin 10
   #ifdef VARIABLE_SPINDLE // Z Limit pin and spindle enabled swapped to access hardware PWM on Pin 11.
-    #define Z_LIMIT_BIT	   4 // Uno Digital Pin 12
+    #define Z_LIMIT_BIT	   4 // Nano Digital Pin 12
   #else
-    #define Z_LIMIT_BIT    3  // Uno Digital Pin 11
+    #define Z_LIMIT_BIT    3  // Nano Digital Pin 11
   #endif
   #if !defined(ENABLE_DUAL_AXIS)
     #define LIMIT_MASK     ((1<<X_LIMIT_BIT)|(1<<Y_LIMIT_BIT)|(1<<Z_LIMIT_BIT)) // All limit bits
@@ -79,10 +79,10 @@
   #define CONTROL_DDR       DDRC
   #define CONTROL_PIN       PINC
   #define CONTROL_PORT      PORTC
-  #define CONTROL_RESET_BIT         0  // Uno Analog Pin 0
-  #define CONTROL_FEED_HOLD_BIT     1  // Uno Analog Pin 1
-  #define CONTROL_CYCLE_START_BIT   2  // Uno Analog Pin 2
-  #define CONTROL_SAFETY_DOOR_BIT   1  // Uno Analog Pin 1 NOTE: Safety door is shared with feed hold. Enabled by config define.
+  #define CONTROL_RESET_BIT         0  // Nano Analog Pin 0
+  #define CONTROL_FEED_HOLD_BIT     1  // Nano Analog Pin 1
+  #define CONTROL_CYCLE_START_BIT   2  // Nano Analog Pin 2
+  #define CONTROL_SAFETY_DOOR_BIT   1  // Nano Analog Pin 1 NOTE: Safety door is shared with feed hold. Enabled by config define.
   #define CONTROL_INT       PCIE1  // Pin change interrupt enable pin
   #define CONTROL_INT_vect  PCINT1_vect
   #define CONTROL_PCMSK     PCMSK1 // Pin change interrupt register
@@ -93,7 +93,7 @@
   #define PROBE_DDR       DDRC
   #define PROBE_PIN       PINC
   #define PROBE_PORT      PORTC
-  #define PROBE_BIT       5  // Uno Analog Pin 5
+  #define PROBE_BIT       5  // Nano Analog Pin 5
   #define PROBE_MASK      (1<<PROBE_BIT)
 
   #if !defined(ENABLE_DUAL_AXIS)
@@ -101,10 +101,10 @@
     // Define flood and mist coolant enable output pins.
     #define COOLANT_FLOOD_DDR   DDRC
     #define COOLANT_FLOOD_PORT  PORTC
-    #define COOLANT_FLOOD_BIT   3  // Uno Analog Pin 3
+    #define COOLANT_FLOOD_BIT   3  // Nano Analog Pin 3
     #define COOLANT_MIST_DDR   DDRC
     #define COOLANT_MIST_PORT  PORTC
-    #define COOLANT_MIST_BIT   4  // Uno Analog Pin 4
+    #define COOLANT_MIST_BIT   4  // Nano Analog Pin 4
 
     // Define spindle enable and spindle direction output pins.
     #define SPINDLE_ENABLE_DDR    DDRB
@@ -113,17 +113,17 @@
     #ifdef VARIABLE_SPINDLE
       #ifdef USE_SPINDLE_DIR_AS_ENABLE_PIN
         // If enabled, spindle direction pin now used as spindle enable, while PWM remains on D11.
-        #define SPINDLE_ENABLE_BIT    5  // Uno Digital Pin 13 (NOTE: D13 can't be pulled-high input due to LED.)
+        #define SPINDLE_ENABLE_BIT    5  // Nano Digital Pin 13 (NOTE: D13 can't be pulled-high input due to LED.)
       #else
-        #define SPINDLE_ENABLE_BIT    3  // Uno Digital Pin 11
+        #define SPINDLE_ENABLE_BIT    3  // Nano Digital Pin 11
       #endif
     #else
-      #define SPINDLE_ENABLE_BIT    4  // Uno Digital Pin 12
+      #define SPINDLE_ENABLE_BIT    4  // Nano Digital Pin 12
     #endif
     #ifndef USE_SPINDLE_DIR_AS_ENABLE_PIN
       #define SPINDLE_DIRECTION_DDR   DDRB
       #define SPINDLE_DIRECTION_PORT  PORTB
-      #define SPINDLE_DIRECTION_BIT   5  // Uno Digital Pin 13 (NOTE: D13 can't be pulled-high input due to LED.)
+      #define SPINDLE_DIRECTION_BIT   5  // Nano Digital Pin 13 (NOTE: D13 can't be pulled-high input due to LED.)
     #endif
 
     // Variable spindle configuration below. Do not change unless you know what you are doing.
@@ -149,23 +149,23 @@
     // NOTE: On the 328p, these must be the same as the SPINDLE_ENABLE settings.
     #define SPINDLE_PWM_DDR   DDRB
     #define SPINDLE_PWM_PORT  PORTB
-    #define SPINDLE_PWM_BIT   3    // Uno Digital Pin 11
-  
+    #define SPINDLE_PWM_BIT   3    // Nano Digital Pin 11
+
   #else
 
-    // Dual axis feature requires an independent step pulse pin to operate. The independent direction pin is not 
-    // absolutely necessary but facilitates easy direction inverting with a Grbl $$ setting. These pins replace 
+    // Dual axis feature requires an independent step pulse pin to operate. The independent direction pin is not
+    // absolutely necessary but facilitates easy direction inverting with a Grbl $$ setting. These pins replace
     // the spindle direction and optional coolant mist pins.
 
     #ifdef DUAL_AXIS_CONFIG_PROTONEER_V3_51
       // NOTE: Step pulse and direction pins may be on any port and output pin.
       #define STEP_DDR_DUAL       DDRC
       #define STEP_PORT_DUAL      PORTC
-      #define DUAL_STEP_BIT       4  // Uno Analog Pin 4
+      #define DUAL_STEP_BIT       4  // Nano Analog Pin 4
       #define STEP_MASK_DUAL      ((1<<DUAL_STEP_BIT))
       #define DIRECTION_DDR_DUAL  DDRC
       #define DIRECTION_PORT_DUAL PORTC
-      #define DUAL_DIRECTION_BIT  3  // Uno Analog Pin 3
+      #define DUAL_DIRECTION_BIT  3  // Nano Analog Pin 3
       #define DIRECTION_MASK_DUAL ((1<<DUAL_DIRECTION_BIT))
 
       // NOTE: Dual axis limit is shared with the z-axis limit pin by default. Pin used must be on the same port
@@ -174,10 +174,10 @@
       #define LIMIT_MASK        ((1<<X_LIMIT_BIT)|(1<<Y_LIMIT_BIT)|(1<<Z_LIMIT_BIT)|(1<<DUAL_LIMIT_BIT))
 
       // Define coolant enable output pins.
-      // NOTE: Coolant flood moved from A3 to A4. Coolant mist not supported with dual axis feature on Arduino Uno.
+      // NOTE: Coolant flood moved from A3 to A4. Coolant mist not supported with dual axis feature on Arduino Nano.
       #define COOLANT_FLOOD_DDR   DDRB
       #define COOLANT_FLOOD_PORT  PORTB
-      #define COOLANT_FLOOD_BIT   5  // Uno Digital Pin 13
+      #define COOLANT_FLOOD_BIT   5  // Nano Digital Pin 13
 
       // Define spindle enable output pin.
       // NOTE: Spindle enable moved from D12 to A3 (old coolant flood enable pin). Spindle direction pin is removed.
@@ -185,9 +185,9 @@
       #define SPINDLE_ENABLE_PORT   PORTB
       #ifdef VARIABLE_SPINDLE
         // NOTE: USE_SPINDLE_DIR_AS_ENABLE_PIN not supported with dual axis feature.
-        #define SPINDLE_ENABLE_BIT    3  // Uno Digital Pin 11
+        #define SPINDLE_ENABLE_BIT    3  // Nano Digital Pin 11
       #else
-        #define SPINDLE_ENABLE_BIT    4  // Uno Digital Pin 12
+        #define SPINDLE_ENABLE_BIT    4  // Nano Digital Pin 12
       #endif
 
       // Variable spindle configuration below. Do not change unless you know what you are doing.
@@ -213,7 +213,7 @@
       // NOTE: On the 328p, these must be the same as the SPINDLE_ENABLE settings.
       #define SPINDLE_PWM_DDR   DDRB
       #define SPINDLE_PWM_PORT  PORTB
-      #define SPINDLE_PWM_BIT   3    // Uno Digital Pin 11
+      #define SPINDLE_PWM_BIT   3    // Nano Digital Pin 11
     #endif
 
     // NOTE: Variable spindle not supported with this shield.
@@ -221,11 +221,11 @@
       // NOTE: Step pulse and direction pins may be on any port and output pin.
       #define STEP_DDR_DUAL       DDRB
       #define STEP_PORT_DUAL      PORTB
-      #define DUAL_STEP_BIT       4  // Uno Digital Pin 12
+      #define DUAL_STEP_BIT       4  // Nano Digital Pin 12
       #define STEP_MASK_DUAL      ((1<<DUAL_STEP_BIT))
       #define DIRECTION_DDR_DUAL  DDRB
       #define DIRECTION_PORT_DUAL PORTB
-      #define DUAL_DIRECTION_BIT  5  // Uno Digital Pin 13
+      #define DUAL_DIRECTION_BIT  5  // Nano Digital Pin 13
       #define DIRECTION_MASK_DUAL ((1<<DUAL_DIRECTION_BIT))
 
       // NOTE: Dual axis limit is shared with the z-axis limit pin by default.
@@ -233,16 +233,16 @@
       #define LIMIT_MASK        ((1<<X_LIMIT_BIT)|(1<<Y_LIMIT_BIT)|(1<<Z_LIMIT_BIT)|(1<<DUAL_LIMIT_BIT))
 
       // Define coolant enable output pins.
-      // NOTE: Coolant flood moved from A3 to A4. Coolant mist not supported with dual axis feature on Arduino Uno.
+      // NOTE: Coolant flood moved from A3 to A4. Coolant mist not supported with dual axis feature on Arduino Nano.
       #define COOLANT_FLOOD_DDR   DDRC
       #define COOLANT_FLOOD_PORT  PORTC
-      #define COOLANT_FLOOD_BIT   4  // Uno Analog Pin 4
+      #define COOLANT_FLOOD_BIT   4  // Nano Analog Pin 4
 
       // Define spindle enable output pin.
       // NOTE: Spindle enable moved from D12 to A3 (old coolant flood enable pin). Spindle direction pin is removed.
       #define SPINDLE_ENABLE_DDR    DDRC
       #define SPINDLE_ENABLE_PORT   PORTC
-      #define SPINDLE_ENABLE_BIT    3  // Uno Analog Pin 3
+      #define SPINDLE_ENABLE_BIT    3  // Nano Analog Pin 3
     #endif
 
   #endif
